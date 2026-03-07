2c93638c2daaa7c33f47bafd3027f7626093c3e0
Tushar Lakhanpal <tusharlakhanpal285@gmail.com>
2026-03-07 20:47:32 +0530

    API KEY UPDATED

--- lib/services/api_keys.dart
+++ lib/services/api_keys.dart
@@ -1,4 +1,4 @@
 class ApiKeys {
-  static const String geminiApiKey = "AIzaSyDt0MBpog7YBD6Euj7Tu3Zw7VVW62lJsyg";
+  static const String geminiApiKey = "AIzaSyACNq7pL_pXjR_F-W7e3K0UsQUHmytVGS8";
   static const String weatherApiKey = "4806695943cf230d3ba18494d4e141d7";
 }
